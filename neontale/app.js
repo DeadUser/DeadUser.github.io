e8b793b200b34f927b58c79cec414e24c7139840
Egor Domozhirov <egor.domozhirov@gmail.com>
2019-11-09 01:44:36 +0300

    123

--- neontale/app.js
+++ neontale/app.js
@@ -1,37 +1,58 @@
 var app = angular.module('app', ['ngRoute']);
 
-app.config(function ($routeProvider) {
-    $routeProvider
-
-        .when('/', {
+app.config(function($routeProvider) {
+    $routeProvider.when('/', {
             templateUrl: 'pages/home.html',
             controller: 'mainController'
         })
-
         .when('/about', {
             templateUrl: 'pages/about.html',
             controller: 'aboutController'
         })
-
         .when('/contact', {
             templateUrl: 'pages/contact.html',
             controller: 'contactController'
         });
 });
 
-app.controller('bodyController', function ($scope) {
-    $scope.text = 'Пример';
+app.controller('mainController', function($scope) {
     $scope.cost = 0;
+    $scope.fonts = [
+        `'Pacifico', cursive`,
+        `'Caveat', cursive`,
+        `'Poiret One', cursive`,
+        `'Russo One', sans-serif`,
+        `'Marck Script', cursive`,
+        `'Bad Script', cursive`,
+        `'Press Start 2P', cursive`,
+        `'Times New Roman', Times, serif`,
+        `Arial, Helvetica, sans-serif`,
+        `'Courier New', Courier, monospace`,
+        `Alpha Echo`
+    ];
+    $scope.text = {
+        value: 'Example',
+        font: $scope.fonts[0],
+        color: '631F00',
+        size: 50
+    };
 
-    $scope.$watch("text", function () {
-        $scope.cost = $scope.text.length * 15;
+    $scope.$watchGroup(['text.value', 'text.font', 'text.color', 'text.size'], function() {
+        $scope.style = {
+            'font-family': $scope.text.font,
+            'color': `#${$scope.text.color}`,
+            'font-size': `${$scope.text.size}px`,
+            'font-style': `${$scope.text.bold? 'italic': 'normal'},`,
+            'font-weight': `${$scope.text.bold? 'bold' : 'normal'}`
+        };
+        $scope.cost = $scope.text.value.length * 1500;
     });
 });
 
-app.controller('aboutController', function ($scope) {
+app.controller('aboutController', function($scope) {
     $scope.message = '';
 });
 
-app.controller('contactController', function ($scope) {
+app.controller('contactController', function($scope) {
     $scope.message = '';
 });
